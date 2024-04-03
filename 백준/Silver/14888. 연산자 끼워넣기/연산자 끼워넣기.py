3db90f219bdf661c18373153c54940d772335df3
Jangwoo No <86145775+YesJW@users.noreply.github.com>
2024-04-03 15:47:30 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 60 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
@@ -1,31 +1,35 @@
 import sys
 
 input = sys.stdin.readline
-N = int(input())
-num = list(map(int, input().split()))
-op = list(map(int, input().split()))  # +, -, *, //
 
-maximum = -1e9
+n = int(input())
+number = list(map(int, input().split()))
+
+# + - * %
+op = list(map(int, input().split()))
+
 minimum = 1e9
+maximum = -1e9
 
 
-def dfs(depth, total, plus, minus, multiply, divide):
-    global maximum, minimum
-    if depth == N:
-        maximum = max(total, maximum)
+def dfs(depth, p, m, mul, div, total):
+    global minimum
+    global maximum
+    if depth == n:
         minimum = min(total, minimum)
+        maximum = max(total, maximum)
         return
 
-    if plus:
-        dfs(depth + 1, total + num[depth], plus - 1, minus, multiply, divide)
-    if minus:
-        dfs(depth + 1, total - num[depth], plus, minus - 1, multiply, divide)
-    if multiply:
-        dfs(depth + 1, total * num[depth], plus, minus, multiply - 1, divide)
-    if divide:
-        dfs(depth + 1, int(total / num[depth]), plus, minus, multiply, divide - 1)
+    if p:
+        dfs(depth + 1, p - 1, m, mul, div, total + number[depth])
+    if m:
+        dfs(depth + 1, p, m - 1, mul, div, total - number[depth])
+    if mul:
+        dfs(depth + 1, p, m, mul - 1, div, total * number[depth])
+    if div:
+        dfs(depth + 1, p, m, mul, div - 1, int(total / number[depth]))
 
 
-dfs(1, num[0], op[0], op[1], op[2], op[3])
+dfs(1, op[0], op[1], op[2], op[3], number[0])
 print(maximum)
 print(minimum)
