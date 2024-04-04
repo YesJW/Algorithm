1c643ad3df24476c71cbf1684e622fc9117ebb54
Jangwoo No <86145775+YesJW@users.noreply.github.com>
2024-04-05 01:05:34 +0900

    [level 1] Title: 모의고사, Time: 3.78 ms, Memory: 10.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/42840. 모의고사/모의고사.py
@@ -0,0 +1,19 @@
+def solution(answers):
+    answer = []
+    p1 = [1,2,3,4,5]
+    p2 = [2,1,2,3,2,4,2,5]
+    p3 = [3,3,1,1,2,2,4,4,5,5]
+    ans = [0,0,0]
+    c1 = c2 = c3 = 0
+    for i in range(len(answers)):
+        if p1[i%len(p1)] == answers[i]:
+            ans[0] += 1
+        if p2[i%len(p2)] == answers[i]:
+            ans[1] += 1
+        if p3[i%len(p3)] == answers[i]:
+            ans[2] += 1
+    max_n = max(ans)
+    answer = list(filter(lambda x: ans[x] == max_n , range(len(ans))))
+    for i in range(len(answer)):
+        answer[i] = answer[i]+1
+    return answer
