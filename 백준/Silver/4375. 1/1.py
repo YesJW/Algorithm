706914f000319b571498e40783820aa56b8bdfa0
Jangwoo No <86145775+YesJW@users.noreply.github.com>
2024-04-04 15:25:49 +0900

    [Silver III] Title: 1, Time: 160 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/4375. 1/1.py
@@ -0,0 +1,19 @@
+import sys
+
+input = sys.stdin.readline
+
+while True:
+
+    try:
+        n = int(input())
+    except:
+        break
+
+    num = 1
+
+    while True:
+        if num % n == 0:
+            print(len(str(num)))
+            break
+        else:
+            num = num * 10 + 1
